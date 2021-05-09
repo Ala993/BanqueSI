05ba27e7970882a6537c3f7174263d0bcb6d8fba
aladr <aladr@192.168.1.4>
2021-05-09 23:58:50 +0100

    third stage

--- BanqueSI/src/main/java/org/glsid/dao/EmployeRepository.java
+++ BanqueSI/src/main/java/org/glsid/dao/EmployeRepository.java
@@ -3,6 +3,6 @@ package org.glsid.dao;
 import org.glsid.entities.Employe;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmplyeRepository extends JpaRepository<Employe, Long>{
+public interface EmployeRepository extends JpaRepository<Employe, Long>{
 
 }
--- BanqueSI/src/main/java/org/glsid/entities/Employe.java
+++ BanqueSI/src/main/java/org/glsid/entities/Employe.java
@@ -12,6 +12,10 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonSetter;
 @Entity 
 public class Employe implements Serializable {
 	@Id
@@ -36,15 +40,18 @@ public class Employe implements Serializable {
 	public void setNomEmploye(String nomEmploye) {
 		this.nomEmploye = nomEmploye;
 	}
+	@JsonIgnore
 	public Employe getEmployeSup() {
 		return employeSup;
 	}
+	@JsonSetter
 	public void setEmployeSup(Employe employeSup) {
 		this.employeSup = employeSup;
 	}
+	@JsonIgnore
 	public Collection<Groupe> getGroupes() {
 		return groupes;
-	}
+	}	
 	public void setGroupes(Collection<Groupe> groupes) {
 		this.groupes = groupes;
 	}
